ed21afb5c65ee9be895625ffb36d093286d55447
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-01-16 22:34:31 +0400

    feat: add backslash parsing

--- parser.go
+++ parser.go
@@ -3,24 +3,44 @@ package main
 import "strings"
 
 func parseArgs(input string) []string {
+	input = strings.TrimLeft(input, " \t\n")
+	if len(input) == 0 {
+		return nil
+	}
+
 	var current strings.Builder
-	result := []string{}
-	isSingle := false
-	isDouble := false
+	var result []string
+
+	var (
+		isSingle  bool
+		isDouble  bool
+		isEscaped bool
+	)
 
 	for _, ch := range input {
 		switch {
-		case ch == '\'' && !isDouble:
+		case ch == '\\' && !isSingle:
+			if isEscaped {
+				current.WriteRune(ch)
+				isEscaped = false
+			} else {
+				isEscaped = true
+			}
+		case ch == '\'' && !isDouble && !isEscaped:
 			isSingle = !isSingle
-		case ch == '"' && !isSingle:
+		case ch == '"' && !isSingle && !isEscaped:
 			isDouble = !isDouble
-		case (ch == ' ' || ch == '\t') && !isSingle && !isDouble:
+		case (ch == ' ' || ch == '\t' || ch == '\n') && !isSingle && !isDouble && !isEscaped:
 			if current.Len() > 0 {
 				result = append(result, current.String())
 				current.Reset()
 			}
 		default:
+			if isEscaped && isDouble && ch != '"' && ch != '\\' {
+				current.WriteRune('\\')
+			}
 			current.WriteRune(ch)
+			isEscaped = false
 		}
 	}
 
